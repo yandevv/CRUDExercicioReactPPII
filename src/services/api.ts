49a079e23a548bbf9d93bf11ffa1e1a2cf5be4bd
Yan Victor <ybritogomes@gmail.com>
2024-09-08 20:27:36 -0300

    feat(api): Added api file to operate database json

---
+++ src/services/api.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+import Book from '../types/BookType';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001'
+})
+
+export const getBooks = () => api.get('/books');
+export const getBookById = (id: number) => api.get(`/books/${id}`);
+export const insertBook = (book: Book) => api.post('/books', book);
+export const updateBook = (id: number, book: Book) => api.put(`/books/${id}`, book);
+export const deleteBook = (id: number) => api.delete(`/books/${id}`);
