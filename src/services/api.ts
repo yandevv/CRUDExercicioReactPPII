6835495c62528bb9b652c940e842c3ff9c95eed4
YanDevv <ybritogomes@gmail.com>
2024-09-09 18:33:32 -0300

    style(api.ts): changed indentation to tab

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Book from '../types/BookType';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001'
+	baseURL: 'http://localhost:3001'
 })
 
 export const getBooks = () => api.get('/books');
