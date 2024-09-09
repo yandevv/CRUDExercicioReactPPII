c96ddbb3a09ce45da4b9bbb13482cf6994a5a9b1
YanDevv <ybritogomes@gmail.com>
2024-09-09 18:32:38 -0300

    feat(home): Added a new home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import '../styles/Home.css';
+
+import LastBooks from '../components/LastBooks'
 
 function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Book Store</h1>
-    </div>
-  )
+	return (
+		<div>
+			<h1>Welcome to the Book Store!</h1>
+			<div className="operationsDiv">
+				<div className="buttonsDiv">
+					<button>Register a book</button>
+					<button>Edit a book</button>
+				</div>
+				<LastBooks />
+			</div>
+		</div>
+	)
 }
 
 export default Home
